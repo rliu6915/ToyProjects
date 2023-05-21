6b08aad61e6f70e2c04ac66d536268967cd212c8
rliu6915 <rliu6915@connect.hku.hk>
2023-05-21 09:47:38 +0800

    course information

---
+++ course-info/src/App.js
@@ -0,0 +1,86 @@
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const part1 = 'Fundamentals of React'
+//   const exercises1 = 10
+//   const part2 = 'Using props to pass data'
+//   const exercises2 = 7
+//   const part3 = 'State of a component'
+//   const exercises3 = 14
+
+//   return (
+//     <div>
+//       <h1>{course}</h1>
+//       <p>
+//         {part1} {exercises1}
+//       </p>
+//       <p>
+//         {part2} {exercises2}
+//       </p>
+//       <p>
+//         {part3} {exercises3}
+//       </p>
+//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//     </div>
+//   )
+// }
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part.name} {props.part.exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.content[0]}/>
+      <Part part={props.content[1]}/>
+      <Part part={props.content[2]}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.exercises[0].exercises + 
+      props.exercises[1].exercises + props.exercises[2].exercises}</p>
+  )
+}
+
+const App = () => {
+  // const-definitions
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content content={course.parts} />
+      <Total exercises={course.parts}/>
+    </div>
+  )
+}
+
+
+export default App
