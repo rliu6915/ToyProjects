097f32ae3252152608d41f041b72ca84d083910d
rliu6915 <rliu6915@connect.hku.hk>
2023-07-10 16:21:00 +0800

    backend for bloglist & unit tests

---
+++ bloglist-backend/app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+
+const app = express()
+const cors = require('cors')
+
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+
+mongoose.set('strictQuery', false)
+
+const mongoUrl = config.MONGODB_URI
+// console.log('mongoUrl', mongoUrl)
+logger.info('connecting to', mongoUrl)
+
+mongoose.connect(mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
