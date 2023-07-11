709c53aa8dae94941513d892aea6c9a2e2e0d26d
rliu6915 <rliu6915@connect.hku.hk>
2023-07-11 15:46:25 +0800

    fix small bug

--- bloglist-backend/utils/logger.js
+++ bloglist-backend/utils/logger.js
@@ -1,8 +1,8 @@
 const info = (...params) => {
-  // console.log(...params)
   if (process.env.NODE_ENV !== 'test') {
     console.log(...params)
   }
+  // console.log(...params)
 }
 
 const error = (...params) => {
