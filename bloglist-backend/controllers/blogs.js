bdfba2a715bdb0c4877c4cebb7b01a01b74d218c
rliu6915 <rliu6915@connect.hku.hk>
2023-07-11 15:49:04 +0800

    change and add endpoints for blogs

--- bloglist-backend/controllers/blogs.js
+++ bloglist-backend/controllers/blogs.js
@@ -1,23 +1,47 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+  if (!blog.likes) {
+    blog.likes = 0
+  }
+  const saveBlog = await blog.save()
+  response.status(201).json(saveBlog)
+  // try {
+  //   const saveBlog = await blog.save()
+  //   response.status(201).json(saveBlog)
+  // } catch (exception) {
+  //   next(exception)
+  // }
 
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result)
-    })
-    .catch((err) => next(err))
+  // blog
+  //   .save()
+  //   .then((result) => {
+  //     response.status(201).json(result)
+  //   })
+  //   .catch((err) => next(err))
+})
+
+blogsRouter.delete('/:id', async (req, res) => {
+  await Blog.findByIdAndRemove(req.params.id)
+  res.status(204).end()
+})
+
+blogsRouter.put('/:id', async (req, res) => {
+  const blog = {
+    title: req.body.title,
+    author: req.body.author,
+    url: req.body.url,
+    likes: req.body.likes || 0,
+  }
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
+  res.json(updatedBlog)
 })
 
 module.exports = blogsRouter
