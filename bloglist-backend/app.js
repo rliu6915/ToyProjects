590ca786060369b51139cafc4df12c9ad016e4bc
rliu6915 <rliu6915@connect.hku.hk>
2023-07-13 10:19:12 +0800

    add user router

--- bloglist-backend/app.js
+++ bloglist-backend/app.js
@@ -5,7 +5,10 @@ const cors = require('cors')
 
 const mongoose = require('mongoose')
 require('express-async-errors')
+
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
@@ -30,6 +33,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
