b46b8c22182dc905631447bfb46ace240b1c44cc
rliu6915 <rliu6915@connect.hku.hk>
2023-07-13 17:19:43 +0800

    middlewares for token and user extracotrs

--- bloglist-backend/utils/middleware.js
+++ bloglist-backend/utils/middleware.js
@@ -1,5 +1,7 @@
 /* eslint-disable consistent-return */
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -36,11 +38,49 @@ const errorHandler = (error, request, response, next) => {
       })
   }
 
+  if (error.name === 'JsonWebTokenError') {
+    return response
+      .status(401)
+      .json({
+        error: 'invalid token',
+      })
+  }
+
+  if (error.name === 'TokenExpiredError') {
+    return response
+      .status(401)
+      .json({
+        error: 'token expired',
+      })
+  }
+
   next(error)
+}
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  // console.log('decodedToken', decodedToken)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  request.user = await User.findById(decodedToken.id)
+  next()
 }
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
+  userExtractor
 }
