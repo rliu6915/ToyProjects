92fdb3e0faac52beae7378cbe44ff70e4cfd9030
rliu6915 <rliu6915@connect.hku.hk>
2023-07-13 17:17:40 +0800

    add login router & add extractors

--- bloglist-backend/app.js
+++ bloglist-backend/app.js
@@ -8,6 +8,7 @@ require('express-async-errors')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const logger = require('./utils/logger')
 const config = require('./utils/config')
@@ -31,9 +32,12 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
+// app.use(middleware.userExtractor)
 
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', middleware.userExtractor, blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
