2b39a5f8b06190108b46581857b1ca924d7341ed
rliu6915 <rliu6915@connect.hku.hk>
2023-07-17 17:42:30 +0800

    add login form

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -4,6 +4,8 @@ import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,8 +13,37 @@ const App = () => {
     )  
   }, [])
 
+  const handelLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
+
   return (
     <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handelLogin}>
+        <div>
+          username
+          <input
+            type='text'
+            value={username}
+            name='Username'
+            onChange={({target}) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type='password'
+            value={password}
+            name='Password'
+            onChange={({target}) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>
+          login
+        </button>
+      </form>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
