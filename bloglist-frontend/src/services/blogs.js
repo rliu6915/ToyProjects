8655c9b66282a5d065cd0011855aaf70c79fa841
rliu6915 <rliu6915@connect.hku.hk>
2023-07-17 17:43:04 +0800

    add login service

--- bloglist-frontend/src/services/blogs.js
+++ bloglist-frontend/src/services/blogs.js
@@ -1,10 +1,15 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
+
+const login = async (credentials) => {
+  const response = await axios.post('/api/login', credentials)
+  return response.data
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll }
+export default { getAll, login}
