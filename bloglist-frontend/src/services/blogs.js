26d5dd432c2f1ae0adaceb4a0aa1cdda170fef54
rliu6915 <rliu6915@connect.hku.hk>
2023-07-18 17:43:56 +0800

    add set toekn in service for create a blog

--- bloglist-frontend/src/services/blogs.js
+++ bloglist-frontend/src/services/blogs.js
@@ -1,10 +1,21 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll = async () => {
   const response = await axios.get(baseUrl)
   return response.data
 }
 
+const create = async (newBlog) => {
+  const response = await axios.post(baseUrl, newBlog, {headers: {Authorization: token}})
+  return response.data
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll }
+export default { getAll, setToken, create}
