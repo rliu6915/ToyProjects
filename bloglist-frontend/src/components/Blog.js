fac70e3e1e5c1b99d9b6318d45748bb0beb43184
rliu6915 <rliu6915@connect.hku.hk>
2023-07-20 10:57:10 +0800

    add inline style into blog component

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,30 @@
-const Blog = ({blog}) => (
-  <li className="blog">
-    {blog.title}
-  </li>  
-)
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div className="blog" style={blogStyle}>
+    <div>
+      {blog.title}
+      <button>view</button>
+    </div>
+    <div>
+      {blog.author}
+    </div>
+    <div>
+      {blog.url}
+    </div>
+    <div>
+      {blog.likes}
+      <button>like</button>
+    </div>
+  </div>  
+  )
+}
 
 export default Blog
