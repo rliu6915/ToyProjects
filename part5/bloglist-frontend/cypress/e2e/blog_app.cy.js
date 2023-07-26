cc7253a8b6054df4a915dcff0daff79261c0d62f
rliu6915 <rliu6915@connect.hku.hk>
2023-07-26 18:02:42 +0800

    5.20 make a test for user likes a blog

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,13 +1,13 @@
 describe('Blog app', function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
     const user = {
       name: 'test name',
       username: 'test username',
       password: 'test password'
     }
-    cy.request('POST', 'http://localhost:3003/api/users/', user)
-    cy.visit('http://localhost:3000')
+    cy.request('POST', `${Cypress.env('BACKEND')}/users/`, user)
+    cy.visit('')
   })
 
   it('Login form is shown', function() {
@@ -31,18 +31,31 @@ describe('Blog app', function() {
       cy.get('#password').type('wrong password')
       cy.get('#login-button').click()
 
-      cy.contains('Wrong username or password')
+      // cy.contains('Wrong username or password')
+      cy.get('.error')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', 'test name logged in')
     })
   })
 
   describe('when logged in', function() {
     beforeEach(function() {
-      cy.contains('login').click()
-      cy.get('#username').type('test username')
-      cy.get('#password').type('test password')
-      cy.get('#login-button').click()
+      cy.login({ username: 'test username', password: 'test password' })
+      // cy.request('POST', 'http://localhost:3003/api/login', {
+      //   username: 'test username', password: 'test password'
+      // }).then((response) => {
+      //   localStorage.setItem('loggedBlogappUser', JSON.stringify(response.body))
+      //   cy.visit('http://localhost:3000')
+      // })
+      // cy.contains('login').click()
+      // cy.get('#username').type('test username')
+      // cy.get('#password').type('test password')
+      // cy.get('#login-button').click()
 
-      cy.contains('test name logged in')
+      // cy.contains('test name logged in')
     })
 
     it('a blog can be created', function() {
@@ -54,5 +67,25 @@ describe('Blog app', function() {
 
       cy.contains('test title')
     })
+
+    describe('and a blog exists', function () {
+      beforeEach(function () {
+        // cy.contains('create blog').click()
+        // cy.get('#title').type('test title 2')
+        // cy.get('#author').type('test author 2')
+        // cy.get('#url').type('test url 2')
+        // cy.get('#create-button').click()
+        cy.createBlog({
+          title: 'test title 2', author: 'test author 2', url: 'test url 2'
+        })
+      })
+
+      it('it can be liked', function () {
+        cy.contains('view').click()
+        cy.contains('like').click()
+        cy.contains('1')
+      })
+    })
   })
+
 })
