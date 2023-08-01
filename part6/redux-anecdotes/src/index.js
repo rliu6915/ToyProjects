92976d38feffaa12ca9f9af7eed4c80f62fab978
rliu6915 <rliu6915@connect.hku.hk>
2023-08-01 13:12:06 +0800

    6.10 use configureStore

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { combineReducers, legacy_createStore as createStore } from 'redux'
+// import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
-import anecdoteReducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
+// import anecdoteReducer from './reducers/anecdoteReducer'
+// import filterReducer from './reducers/filterReducer'
+import store from './store'
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer,
-})
+// const reducer = combineReducers({
+//   anecdotes: anecdoteReducer,
+//   filter: filterReducer,
+// })
 
-const store = createStore(reducer)
+// const store = configureStore({
+//   reducer: {
+//     anecdotes: anecdoteReducer,
+//     filter: filterReducer,
+//   }
+// })
 
 console.log(store.getState())
 
