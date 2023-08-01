b36108a256987a870ee5b0101e289382294240eb
rliu6915 <rliu6915@connect.hku.hk>
2023-08-01 13:22:10 +0800

    6.10 refector code to use createSlice

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,30 @@
-const filterReducer = (state = 'ALL', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
+import { createSlice } from '@reduxjs/toolkit'
+
+// const filterReducer = (state = 'ALL', action) => {
+//   switch (action.type) {
+//     case 'SET_FILTER':
+//       return action.payload
+//     default:
+//       return state
+//   }
+// }
+
+// export const filterChange = (filter) => {
+//   return {
+//     type: 'SET_FILTER',
+//     payload: filter,
+//   }
+// }
+
+const filerSlice = createSlice({
+  name: 'filter',
+  initialState: 'ALL',
+  reducers: {
+    filterChange(state, action) {
       return action.payload
-    default:
-      return state
+    }
   }
-}
+})
 
-export const filterChange = (filter) => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter,
-  }
-}
-
-export default filterReducer
+export const { filterChange } = filerSlice.actions
+export default filerSlice.reducer
