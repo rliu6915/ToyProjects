70cc2fd81b25b38097040ae12fdab664116ff9e7
rliu6915 <rliu6915@connect.hku.hk>
2023-08-09 15:54:35 +0800

    7.9

--- part7/bloglist-frontend/src/store.js
+++ part7/bloglist-frontend/src/store.js
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit'
 import blogReducer from './reducers/blogReducer'
 import notificationReducer from './reducers/notificationReducer'
 
+
+
 const store = configureStore({
   reducer: {
     blogs: blogReducer,
