3dd57edbe807d5f4b13f7a7e807ef61080ce05f0
rliu6915 <rliu6915@connect.hku.hk>
2023-08-09 17:19:05 +0800

    7.12/delete a blog/reducer

--- part7/bloglist-frontend/src/reducers/blogReducer.js
+++ part7/bloglist-frontend/src/reducers/blogReducer.js
@@ -12,17 +12,27 @@ const blogSlice = createSlice({
     setBlogs(state, action) {
       // console.log('action.payload', action.payload)
       return action.payload
+    },
+    removeBlog(state, action) {
+      return state.filter(blog => blog.id !== action.payload)
     }
   }
 })
 
-export const { appendBlog, setBlogs } = blogSlice.actions
+export const { appendBlog, setBlogs, removeBlog } = blogSlice.actions
 
 export const initializeBlogs = () => {
   return async (dispatch) => {
     const blogs = await blogService.getAll()
     // console.log('blogs', blogs)
     dispatch(setBlogs(blogs))
+  }
+}
+
+export const deleteBlog = (id) => {
+  return async (dispatch) => {
+    await blogService.remove(id)
+    dispatch(removeBlog(id))
   }
 }
 
