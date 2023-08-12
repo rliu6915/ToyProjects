cc0f013057f9d2f478f756a474653531bd0b6e03
rliu6915 <rliu6915@connect.hku.hk>
2023-08-12 16:26:26 +0800

    7.16/routing/link in home

--- part7/bloglist-frontend/src/components/Blog.js
+++ part7/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,6 @@
-import { useSelector } from 'react-redux'
-import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Blog = ({ blog }) => {
-  const loginUser = useSelector(state => state.loginUser)
-  // console.log('loginUser in BL', loginUser)
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -11,46 +8,15 @@ const Blog = ({ blog }) => {
     borderWidth: 1,
     marginBottom: 5
   }
-  // const [visible, setVisible] = useState(false)
-
-  // const hideWhenVisible = { display: visible ? 'none' : '' }
-  // const showWhenVisible = { display: visible ? '' : 'none' }
-
-  // const handleViewChange = () => {
-  //   setVisible(!visible)
-  // }
-
-  // console.log('blog.user.username', blog.user.name)
-  // console.log('loginUser.username', loginUser.username)
 
   return (
-    // <div className="blog" style={blogStyle}>
-    //   {/* <div className="shortBlog" style={hideWhenVisible}>
-    //     {blog.title} {blog.author}
-    //     <button onClick={handleViewChange}>view</button>
-    //   </div>
-    //   <div className="longBlog" style={showWhenVisible}> */}
-    //     {/* <div>
-    //       {blog.title}
-    //       <button onClick={handleViewChange}>hide</button>
-    //     </div> */}
-    //     {/* <div>
-    //       {blog.url}
-    // // </div>        </div> */}
-    //     {/* <div>
-    //       {blog.likes}
-    //       <button className="likeButton" onClick={hanldeLikeChange}>like</button>
-    //     </div> */}
-    //     {/* <div>
-    //       {blog.user.name}
-    //     </div> */}
-    //     {/* {blog.user.username === loginUser.username && (
-    //       <div>
-    //         <button onClick={handelBlogDelete}>remove</button>
-    //       </div>
-    //     )} */}
-    //   </div>
-    null
+    <>
+      <div style={blogStyle}>
+        <div>
+          <Link to={`blogs/${blog.id}`}>{blog.title} {blog.author}</Link>
+        </div>
+      </div>
+    </>
   )
 }
 
--- part7/bloglist-frontend/src/components/BlogList.js
+++ part7/bloglist-frontend/src/components/BlogList.js
@@ -1,33 +1,14 @@
 import { useSelector } from 'react-redux'
 import Blog from './Blog'
 
-import { useDispatch } from 'react-redux'
-import { deleteBlog, likeBlog } from '../reducers/blogReducer'
-
-
-
 const BlogList = () => {
   // const blogsFiltered = blogs.filter(blog => blog.user.username === user.username)
   // console.log(blogsFiltered)
-
-  const dispatch = useDispatch()
 
   const blogs = useSelector(state => state.blogs)
   // console.log('blogs', blogs)
   const copies = [...blogs]
   copies.sort((a, b) => b.likes - a.likes)
-
-  // // like
-  // const blogLike = (blogObject) => {
-  //   dispatch(likeBlog(blogObject.id, blogObject))
-  // }
-
-  // // delete blog
-  // const blogDelete = (blogObject) => {
-  //   if (window.confirm(`Remove blog ${blogObject.title} by ${blogObject.author}`)) {
-  //     dispatch(deleteBlog(blogObject.id))
-  //   }
-  // }
 
   return (
     <div>
