e1ce0479de5d3627c6b1f7c42eb0502a0f7fd837
rliu6915 <rliu6915@connect.hku.hk>
2023-08-18 11:11:24 +0800

    7.19/feature/addcomment/backend

--- part7/bloglist-backend/controllers/comments.js
+++ part7/bloglist-backend/controllers/comments.js
@@ -9,5 +9,19 @@ commentsRouter.get("/:id/comments", async (request, response) => {
   response.json(comments)
 })
 
+commentsRouter.post("/:id/comments", async (request, response) => {
+  const body = request.body
+  const id = request.params.id
+  const blog = await Blog.findById(id)
+  const comment = new Comment({
+    content: body.content,
+    blog: id
+  })
+  const saveComment = await comment.save()
+  blog.comments = blog.comments.concat(saveComment)
+  await blog.save()
+  response.status(201).json(saveComment)
+})
+
 module.exports = commentsRouter
 
