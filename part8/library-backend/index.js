fa5a858b8b09afd5f0e1d6bec105fd42112d9f39
rliu6915 <rliu6915@connect.hku.hk>
2023-10-19 15:44:37 +0800

    8.3: All authors

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -7,29 +7,24 @@ let authors = [
     name: 'Robert Martin',
     id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
     born: 1952,
-    bookCount: 2
   },
   {
     name: 'Martin Fowler',
     id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
     born: 1963,
-    bookCount: 2
   },
   {
     name: 'Fyodor Dostoevsky',
     id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
     born: 1821,
-    bookCount: 2
   },
   { 
     name: 'Joshua Kerievsky', // birthyear not known
     id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-    bookCount: 1
   },
   { 
     name: 'Sandi Metz', // birthyear not known
     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-    bookCount: 1
   },
 ]
 
@@ -155,6 +150,9 @@ const resolvers = {
       }
     },
     allAuthors: () => authors
+  },
+  Author: {
+    bookCount: root => books.filter(book => book.author === root.name).length
   },
   Mutation: {
     addBook: (root, args) => {
