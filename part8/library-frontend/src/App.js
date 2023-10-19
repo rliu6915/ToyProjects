53712f06da98ed5b37aed2ca3075cfb20d6cfc22
rliu6915 <rliu6915@connect.hku.hk>
2023-10-19 22:27:31 +0800

    8.10: Adding a book

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -27,6 +27,23 @@ const ALL_BOOKS = gql`
   }
 `
 
+// const CREATE_BOOK = gql`
+//   mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+//     addBook(
+//       title: $title
+//       author: $author
+//       published: $published
+//       genres: $genres
+//     ) {
+//       title
+//       author
+//       published
+//       genres
+//       id
+//     }
+//   }
+// `
+
 const App = () => {
   const [page, setPage] = useState('authors')
 
