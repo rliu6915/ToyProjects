cf1422e820f26525920e950a202cbbd41233b437
rliu6915 <rliu6915@connect.hku.hk>
2023-10-20 16:59:54 +0800

    8.10/polling

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -9,8 +9,12 @@ import { ALL_AUTHORS, ALL_BOOKS } from './queries'
 const App = () => {
   const [page, setPage] = useState('authors')
 
-  const result = useQuery(ALL_AUTHORS)
-  const result2 = useQuery(ALL_BOOKS)
+  const result = useQuery(ALL_AUTHORS, {
+    pollInterval: 2000
+  })
+  const result2 = useQuery(ALL_BOOKS, {
+    pollInterval: 2000
+  })
 
   if (result.loading || result2.loading) {
     return <div>loading...</div>
