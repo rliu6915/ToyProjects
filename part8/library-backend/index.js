77fc6984e1dfc6d81a5d8428b7acc5c772d35737
rliu6915 <rliu6915@connect.hku.hk>
2023-11-16 09:41:14 +0800

    8.16 user and logging in /add query me

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -100,6 +100,9 @@ const resolvers = {
       // const authors = await Author.find({})
       // console.log(authors)
       return await Author.find({})
+    },
+    me: (root, args, context) => {
+      return context.currentUser
     }
   },
   Author: {
@@ -204,6 +207,17 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.startsWith('Bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), process.env.JWT_SECRET
+      )
+      const currentUser = await User
+        .findById(decodedToken.id)
+      return { currentUser }
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
