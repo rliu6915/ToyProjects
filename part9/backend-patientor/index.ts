eb641d784f6c2496516d903a9cd209027f90d14e
rliu6915 <rliu6915@connect.hku.hk>
2024-06-24 12:03:45 +0800

    9.9: Patientor backend, step2

--- part9/backend-patientor/index.ts
+++ part9/backend-patientor/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
+
+app.use(cors())
 
 app.use(express.json());
 
@@ -8,7 +11,7 @@ app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
