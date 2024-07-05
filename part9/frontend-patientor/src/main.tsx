aaf15db59f1376890fc472c1abe93c2241bf75aa
rliu6915 <rliu6915@connect.hku.hk>
2024-07-05 22:37:14 +0800

    update

--- part9/frontend-patientor/src/main.tsx
+++ part9/frontend-patientor/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
